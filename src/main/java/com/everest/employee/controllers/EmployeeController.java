c05ce0930af7e5d263ffb37223b07a0305e97ec0
BhavanaEE <bhavana.chivukula@everest.engineering>
2022-02-18 14:04:43 +0530

    Updated sorting in search result

--- src/main/java/com/everest/employee/controllers/EmployeeController.java
+++ src/main/java/com/everest/employee/controllers/EmployeeController.java
@@ -5,6 +5,7 @@ import com.everest.employee.models.EmployeesResult;
 import com.everest.employee.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,8 +15,11 @@ public class EmployeeController {
     private final EmployeeService employeeService;
 
     @GetMapping(value = "/search")
-    public EmployeesResult findByName(@RequestParam("query") String searchKeyword, @RequestParam(value = "page", defaultValue = "1") int page,@RequestParam(value = "size", defaultValue = "10") int pageSize) {
-        Page<Employee> employeePage = employeeService.getEmployeeByName(searchKeyword,page,pageSize);
+    public EmployeesResult findByName(@RequestParam("query") String searchKeyword, @RequestParam(value = "page", defaultValue = "1") int page,@RequestParam(value = "size", defaultValue = "10") int pageSize,@RequestParam(value = "sort", defaultValue = "firstName,asc") String[] sortBy) {
+        Sort.Direction direction= Sort.Direction.ASC;
+        if (sortBy[1].equalsIgnoreCase("desc")){ direction= Sort.Direction.DESC;}
+        Sort sort= Sort.by(direction,sortBy[0]);
+        Page<Employee> employeePage = employeeService.getEmployeeByName(searchKeyword,page,pageSize,sort);
         return new EmployeesResult(employeePage);
     }
 }
--- src/main/java/com/everest/employee/services/EmployeeService.java
+++ src/main/java/com/everest/employee/services/EmployeeService.java
@@ -1,12 +1,12 @@
 package com.everest.employee.services;
 
 import com.everest.employee.entities.Employee;
-import com.everest.employee.models.EmployeesResult;
 import com.everest.employee.repositories.JpaEmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,8 +17,8 @@ public class EmployeeService {
 
     private final JpaEmployeeRepository jpaEmployeeRepository;
 
-    public Page<Employee> getEmployeeByName(String name,int page,int pageSize) {
-        Pageable pageable = PageRequest.of(page - 1, pageSize);
+    public Page<Employee> getEmployeeByName(String name, int page, int pageSize, Sort sort) {
+        Pageable pageable = PageRequest.of(page - 1, pageSize,sort);
         return jpaEmployeeRepository.findByFirstNameContainingOrLastNameContaining(name,name,pageable);
     }
 }

118ac326657c193de913140a9bfada135bff3b99
BhavanaEE <bhavana.chivukula@everest.engineering>
2022-02-19 18:39:32 +0530

    Added validation

--- src/main/java/com/everest/employee/controllers/EmployeeController.java
+++ src/main/java/com/everest/employee/controllers/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/employees")
+@RequestMapping("api/employees")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
--- src/main/java/com/everest/employee/entities/Address.java
+++ src/main/java/com/everest/employee/entities/Address.java
@@ -3,6 +3,7 @@ package com.everest.employee.entities;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Setter
 @Getter
@@ -11,13 +12,18 @@ import javax.persistence.*;
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotBlank(message = "Id shouldn't be blank")
     private Long addressId;
-    private Long employeeId;
+    @NotBlank(message = "Address shouldn't be blank")
     private String addressLane1;
     private String addressLane2;
+    @NotBlank(message = "City shouldn't be blank")
     private String city;
+    @NotBlank(message = "State shouldn't be blank")
     private String state;
+    @NotBlank(message = "Zipcode shouldn't be blank")
     private Long zipcode;
+    @NotBlank(message = "Country shouldn't be blank")
     private String country;
 
 }

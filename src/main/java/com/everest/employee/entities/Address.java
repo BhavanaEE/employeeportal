74c1877ed8ae458c938d34acd12d46dde5f78a14
BhavanaEE <bhavana.chivukula@everest.engineering>
2022-02-19 18:42:46 +0530

    Added validation

--- src/main/java/com/everest/employee/entities/Address.java
+++ src/main/java/com/everest/employee/entities/Address.java
@@ -3,21 +3,27 @@ package com.everest.employee.entities;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Setter
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity(name = "Address")
+@Entity
+@Table(name = "Address")
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotBlank(message = "Id shouldn't be blank")
     private Long addressId;
-    private Long employeeId;
+    @NotBlank(message = "Address shouldn't be blank")
     private String addressLane1;
     private String addressLane2;
+    @NotBlank(message = "City shouldn't be blank")
     private String city;
+    @NotBlank(message = "State shouldn't be blank")
     private String state;
+    @NotBlank(message = "Zipcode shouldn't be blank")
     private Long zipcode;
+    @NotBlank(message = "Country shouldn't be blank")
     private String country;
+
 }
--- src/main/java/com/everest/employee/entities/Employee.java
+++ src/main/java/com/everest/employee/entities/Employee.java
@@ -2,32 +2,46 @@ package com.everest.employee.entities;
 
 import lombok.Getter;
 import lombok.Setter;
+
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
-@Table(name = "Employee")
-@Entity
 @Setter
 @Getter
+@Entity
+@Table(name = "Employee")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long employeeId;
+    @NotBlank(message = "Firstname shouldn't be blank")
     private String firstName;
+    @NotBlank(message = "Lastname shouldn't be blank")
     private String lastName;
+    @NotBlank(message = "Email shouldn't be blank")
+    @Email(message = "Email is not valid",regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@$" + "everest.engineering")
     private String everestEmailId;
+    @NotBlank(message = "Mail shouldn't be blank")
+    @Email(message = "Email is not valid",regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
     private String personalMailId;
+    @NotBlank(message = "Password shouldn't be blank")
     private String password;
+    @NotBlank(message = "Date of birth shouldn't be blank")
     private LocalDate dateOfBirth;
+    @NotBlank(message = "Date of join shouldn't be blank")
     private LocalDate dateOfJoin;
+    @NotBlank(message = "Designation shouldn't be blank")
     private String designation;
+    @NotBlank(message = "Experience shouldn't be blank")
     private int experience;
     private String bio;
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "presentAddressId")
+    @NotBlank(message = "Present address shouldn't be blank")
     private Address presentAddress;
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "permanentAddressId")
     private Address permanentAddress;
-
 }

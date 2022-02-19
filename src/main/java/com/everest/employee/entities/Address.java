1e57cfd8f121402a303e2154df726b7f888cb134
BhavanaEE <bhavana.chivukula@everest.engineering>
2022-02-19 20:11:01 +0530

    Added validation

--- src/main/java/com/everest/employee/entities/Address.java
+++ src/main/java/com/everest/employee/entities/Address.java
@@ -3,6 +3,8 @@ package com.everest.employee.entities;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Setter
 @Getter
@@ -11,12 +13,17 @@ import javax.persistence.*;
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull(message = "Id shouldn't be null")
     private Long addressId;
-    private Long employeeId;
+    @NotBlank(message = "Address shouldn't be blank")
     private String addressLane1;
     private String addressLane2;
+    @NotBlank(message = "City shouldn't be blank")
     private String city;
+    @NotBlank(message = "State shouldn't be blank")
     private String state;
-    private Long zipcode;
+    private long zipcode;
+    @NotBlank(message = "Country shouldn't be blank")
     private String country;
+
 }

e6b74fd66dde973a326dbfe1951efdfee1d5aea1
BhavanaEE <bhavana.chivukula@everest.engineering>
2022-02-19 21:17:26 +0530

    Updated Transaction annotation

--- src/main/java/com/everest/employee/services/EmployeeService.java
+++ src/main/java/com/everest/employee/services/EmployeeService.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
